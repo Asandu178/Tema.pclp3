86a1a78e306aa594a397256c896b8f6e2b4cc0ca
Asandu178 <dragos310131@gmail.com>
2025-05-24 15:03:31 +0300

    Analizarea datelor part1

--- main.py
+++ main.py
@@ -23,7 +23,7 @@ def random_gender():
 df = pd.read_csv("chess_games.csv")
 
 # simplific modelul prins stergerea unor campuri fie redundante fie care complica prea mult
-df = df.drop(columns=['opening_fullname', 'opening_variation', 'opening_moves', 'opening_response', 'white_id', 'black_id'])
+df = df.drop(columns=['opening_fullname', 'opening_variation', 'opening_moves', 'opening_response', 'white_id', 'black_id', 'game_id'])
 
 # mapez in fct de castigator la 1, 2 sau 0
 df['winner'] = df['winner'].map({'White': 1, 'Black': 2, 'Draw': 0})
@@ -33,9 +33,6 @@ df['winner'] = df['winner'].map({'White': 1, 'Black': 2, 'Draw': 0})
 df['victory_status'] = df['victory_status'].map({'Mate' : 0, 'Resign' : 1, 'Out of Time' : 2, 'Draw' : 3})
 
 # nu vreau sa folosesc combinatia de miscari pentru ca altfel as stii mereu cine castiga
-# folosim in schimb numarul de mutari totale
-
-df['num_moves'] = df['moves'].str.split().apply(len)
 
 df = df.drop(columns=['moves'])
 
@@ -61,6 +58,31 @@ df['nationality'] = [fake_nationality() for _ in range(length)]
 # generez in mod aleatoriu sexul castigatorilor
 df['gender'] = [random_gender() for _ in range(length)]
 
+# analiza datelor initiale
+
+print(df.describe())
+
+#               turns  victory_status        winner  white_rating  black_rating  initial_time  increment_sec           age
+# count  20058.000000     20058.00000  20058.000000  20058.000000  20058.000000  20058.000000   20058.000000  20058.000000
+# mean      60.465999         0.85876      1.406671   1596.631868   1588.831987     13.824110       5.249626     38.874813
+# std       33.570585         0.74823      0.579682    291.253376    291.036126     17.160179      14.289591     14.751223
+# min        1.000000         0.00000      0.000000    784.000000    789.000000      0.000000       0.000000     14.000000
+# 25%       37.000000         0.00000      1.000000   1398.000000   1391.000000     10.000000       0.000000     26.000000
+# 50%       55.000000         1.00000      1.000000   1567.000000   1562.000000     10.000000       0.000000     39.000000
+# 75%       79.000000         1.00000      2.000000   1793.000000   1784.000000     15.000000       7.000000     52.000000
+# max      349.000000         3.00000      2.000000   2700.000000   2723.000000    180.000000     180.000000     64.000000
+
+# observatie, am analizat direct dataset-ul mare, pentru a incerca sa intuim aproximarile finale
+# observam ca numarul mediu de mutari este 60, avem totusi si meciuri aberant de lungi(maxim 349) pe care le vom elimina
+# media pentru statutul de victory este 0.85 deci tendinta este ca un meci sa se termine fie in mat sau capitulare
+# rating-ul pentru jucatori este in jur de 1590, exista o disperie mare(jucatori intre 784-2700)
+# media timpului(minute) este de 13 minute
+# acesta este incrementat dupa fiecare mutare cu in medie 5.2 secunde
+# varsta medie a jucatorilor este de 38 de ani cu interval intre 14 si 64 de ani
+# tendinta generala arata ca meciurile se termina intr-o victorie a jucatorului alt ca rezultat al mediei 1.4
+# alb - 1, negru - 2, egal - 0
+
+
 # preprocesare
 
 # aplic one-hot encoding pentru rated, opening_shortname, nationality, opening_code
@@ -75,9 +97,9 @@ df_encoded['gender'] = encoder.fit_transform(df_encoded['gender'])
 # standardizarea variabilelor
 scaler = StandardScaler()
 
-transform = scaler.fit_transform(df_encoded[['turns', 'white_rating', 'black_rating', 'age', 'num_moves', 'initial_time', 'increment_sec']])
+transform = scaler.fit_transform(df_encoded[['turns', 'white_rating', 'black_rating', 'age', 'initial_time', 'increment_sec']])
 
-df_encoded[['turns', 'white_rating', 'black_rating', 'age', 'num_moves', 'initial_time', 'increment_sec']] = transform
+df_encoded[['turns', 'white_rating', 'black_rating', 'age', 'initial_time', 'increment_sec']] = transform
 
 # separarea datelor
 
@@ -89,6 +111,9 @@ X_train, X_test, y_train, y_test = train_test_split(
     X, y, test_size=0.2, random_state=42
 )
 
+print(f"Dimensiunea setului de antrenament: {X_train.shape}")
+print(f"Dimensiunea setului de test: {X_test.shape}")
+
 # creez dataframe-urile pt train si test
 train = pd.concat([X_train, y_train], axis=1)
 test = pd.concat([X_test, y_test], axis=1)
@@ -97,5 +122,3 @@ test = pd.concat([X_test, y_test], axis=1)
 
 train.to_csv("train_data.csv", index=False)
 test.to_csv("test_data.csv", index=False)
-
-print(df_encoded.describe())
